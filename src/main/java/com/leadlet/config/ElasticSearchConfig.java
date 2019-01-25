fcef00d07d1f9b61cb49507dcee0d78232443c08
yavuzgok <ygokirmak@uber.com>
2019-01-25 23:57:26 +0100

    Elasticsearch prod configuration without name-password

--- src/main/java/com/leadlet/config/ElasticSearchConfig.java
+++ src/main/java/com/leadlet/config/ElasticSearchConfig.java
@@ -5,16 +5,15 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.common.settings.Settings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.AbstractFactoryBean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 @Configuration
 public class ElasticSearchConfig extends AbstractFactoryBean {
@@ -63,18 +62,16 @@ public class ElasticSearchConfig extends AbstractFactoryBean {
 
     private RestHighLevelClient buildClient() {
         try {
-            Settings settings = Settings.builder()
-                .put("cluster.name", "prod").build();
-
-            final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-            credentialsProvider.setCredentials(AuthScope.ANY,
-                new UsernamePasswordCredentials(username, password));
-
             RestClientBuilder builder = RestClient.builder(new HttpHost(clusterHost, clusterPort, protocol))
-                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
-                    @Override
-                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
-                        return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                .setHttpClientConfigCallback(httpClientBuilder -> {
+                    if(!StringUtils.isEmpty(username) && !StringUtils.isEmpty(password)){
+                        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+                        credentialsProvider.setCredentials(AuthScope.ANY,
+                            new UsernamePasswordCredentials(username, password));
+                        httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                        return httpClientBuilder;
+                    } else {
+                        return httpClientBuilder;
                     }
                 });
 
